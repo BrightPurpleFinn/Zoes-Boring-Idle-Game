0d2353e6a8509206e40ffc2004c41264ab65a3cd
BrightPurpleFinn <zoecfinn@gmail.com>
2025-11-05 10:48:53 +0000

    code refactoring

--- src/stores/upgradesStore.js
+++ src/stores/upgradesStore.js
@@ -15,15 +15,13 @@ export const version = 1;
  * @type {Array<[string, string[], number[], (x: any) => void]>}
  */
 const upgrades = newUpgrades([
+  // name, description, [[currency, cost]], REDUNDNET?, index
   ["Big Upgrade no. One!", "Doubles your gold per second", [["Gold", 100]], ["gold"], 0],
   ["Other big upgrade", "Squares your gold per second", [["Gold", 1000]], ["gold"], 1]
 ]);
 
-console.log();
-
 const initialState = {
   upgrades: upgrades,
-  availableUpgrades: upgrades.map((x) => {return x.index;}),
   broughtUpgrades: []
 };
 
@@ -32,34 +30,32 @@ function config(set, get) {
     ...initialState,
     buyUpgrade: (index) => {
       const s = get();
-      
+
       const upgrade = s.upgrades.find((x) => x.index == index);
       if (!upgrade) {
         throw Error(`Unknown error occured`);
       }
 
-      const {gold, deductGold} = useGoldStore.getState();
+      const { gold, updateGold } = useGoldStore.getState();
 
-      if(!upgrade.cost.every((x) => {
+      if (!upgrade.cost.every((x) => {
         if (x[0] == "Gold") return gold >= x[1];
         throw Error(`Unknown Currency in upgrade ${upgrade.name}`);
       })) return;
 
       upgrade.cost.forEach((x) => {
-        if (x[0] == "Gold") deductGold(x[1]);
+        if (x[0] == "Gold") updateGold(- x[1]);
       });
 
       if (s.broughtUpgrades.includes(index)) {
         throw Error(`Upgrade ${index} already present in broughtUpgrades`);
       }
-      set({ broughtUpgrades: [...s.broughtUpgrades, index]});
-      set({ availableUpgrades: s.availableUpgrades.filter(x => {return x != index;}) });
+      set({ broughtUpgrades: [...s.broughtUpgrades, index] });
+      set({ availableUpgrades: s.availableUpgrades.filter(x => { return x != index; }) });
     },
     getAvailableUpgrades: () => {
       const s = get();
-      return s.upgrades.filter((x) => {
-        return s.availableUpgrades.includes(x.index);
-      });
+      return s.upgrades.filter((x) => !s.broughtUpgrades.includes(x.index));
     },
     isUpgradeActivated: (index) => {
       const s = get();
@@ -80,8 +76,8 @@ function rehydrateHandler(state) {
   console.log(`rehydration: ${s}`);
 }
 
-export function partialize(state) {
-  return state;
+export function partialize({broughtUpgrades}) {
+  return {broughtUpgrades};
 }
 
 export const useUpgradeStore = createBaseStore(
