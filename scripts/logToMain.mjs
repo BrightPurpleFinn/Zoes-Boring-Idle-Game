65fa587bbf4f633375f8e9e07283e0446363e42e
BrightPurpleFinn <zoecfinn@gmail.com>
2025-11-08 21:47:27 +0000

    reset workflow to base ready for future releases

--- scripts/logToMain.mjs
+++ scripts/logToMain.mjs
@@ -1,11 +1,32 @@
 import { readFileSync, writeFileSync, unlinkSync } from "fs"
+import inquirer from "inquirer";
 
 const logReleasePath = "changelogs/logRelease.json"
 const logRelease = JSON.parse(readFileSync(logReleasePath, "utf8"));
 const logPath = "changelogs/log.json"
 const log = JSON.parse(readFileSync(logPath, "utf8"));
 
-log.push(logRelease)
+const {releaseName} = await inquirer.prompt([{
+    type: "input",
+    name: "releaseName",
+    message: "What is the release name:",
+    }
+]);
+
+const newRecord = JSON.parse("{}")
+
+newRecord.releaseName = releaseName
+newRecord.version = JSON.parse(readFileSync("package.json", "utf8")).version;
+
+newRecord.date = new Date();
+newRecord.patches = {fix: [], minor: [], major: []}
+
+for (let x of logRelease) {
+    const patchRecord = {patchTitle: x.patchTitle, changes: x.changes}
+    newRecord.patches[x.patchType].push(patchRecord)
+}
+
+log.push(newRecord)
 console.log(log);
 
 writeFileSync(logPath, JSON.stringify(log, null, 2))
